909c637f1237ec963f1c78d92deb3faa4c0dd32b
Eric Masi <eric@gitmasi.com>
2019-04-17 18:52:19 -0400

    add better pokemon images

--- src/components/Pokecard.js
+++ src/components/Pokecard.js
@@ -2,11 +2,22 @@ import React from 'react';
 import './Pokecard.css';
 
 const pokecard = props => {
+
+  function addZeros(str){
+    let output = str.toString();
+    while (output.length < 3) {
+      output = '0' + output;
+    }
+    console.log(output);
+    return output;
+  }
+
   const {id, name, type, exp} = props;
+  
   return (
     <div className="Pokecard">
       <h3>{name}</h3>
-      <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`} alt={`the pokemon ${name}`}/>
+      <img src={`https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${addZeros(id)}.png`} alt={`the pokemon ${name}`}/>
       <p>{type}</p>
       <p>{exp}</p>
     </div>
