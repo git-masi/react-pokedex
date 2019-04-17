5bafabce49a86b82df5b9b51705050af4ef9929e
Eric Masi <eric@gitmasi.com>
2019-04-17 17:11:21 -0400

    pokedex/pokecard stateless function components

---
+++ src/components/Pokedex.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Pokecard from './Pokecard';
+
+const pokedex = props => {
+  return (
+    <div>
+      <Pokecard/>
+    </div>
+  )
+}
+
+export default pokedex;
