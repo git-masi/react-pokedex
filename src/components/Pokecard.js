f036194dcdc65bac02b6b1d15279bedbff7f956a
Eric Masi <eric@gitmasi.com>
2019-04-17 17:31:02 -0400

    display all pokecard data and add styles to cards

--- src/components/Pokecard.js
+++ src/components/Pokecard.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import './Pokecard.css';
 
 const pokecard = props => {
+  const {id, name, type, exp} = props;
   return (
-    <div>
-      <p>{props.name}</p>
+    <div className="Pokecard">
+      <h3>{name}</h3>
+      <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`}/>
+      <p>{type}</p>
+      <p>{exp}</p>
     </div>
   )
 }
