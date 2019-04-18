b12fe42b8c2109e06e9dca77aaba3c1546d9e51c
Eric Masi <eric@gitmasi.com>
2019-04-17 21:37:00 -0400

    add img background so it stands out

--- src/components/Pokecard.js
+++ src/components/Pokecard.js
@@ -16,7 +16,7 @@ const pokecard = props => {
   const classes = ['Pokecard', row === 'top' ? 'Pokecard--top-row' : 'Pokecard--bottom-row']
 
   return (
-    <div className={classes.join(' ')} style={{animationName: 'show', animationDuration: `${((index * 1000) + 1000) + 'ms'}`, animationFillMode: 'forwards'}}>
+    <div className={classes.join(' ')} style={{animationName: 'show', animationDuration: `${((index * (1000 - (index * 100))) + 1000) + 'ms'}`, animationFillMode: 'forwards'}}>
       <h3>{name}</h3>
       <img className="Pokecard--img" src={`https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${addZeros(id)}.png`} alt={`the pokemon ${name}`}/>
       <p>Type: {type}</p>
