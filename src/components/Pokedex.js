e415308f5825d277f1c07a25170f5992e25d1fce
Eric Masi <eric@gitmasi.com>
2019-04-18 09:28:19 -0400

    add animation to exp box

--- src/components/Pokedex.js
+++ src/components/Pokedex.js
@@ -20,10 +20,20 @@ const pokedex = props => {
 
   return (
     <div className="Pokedex">
-      <div style={{order: props.row === 'top' ? '2' : '0'}} className="Pokedex--card-container">
+      <div
+        style={{
+          order: props.row === 'top' ? '2' : '0'
+        }}
+        className="Pokedex--card-container">
         {pokecardsArr}
       </div>
-      <p style={{order: props.row === 'top' ? '1' : '0'}} className={props.highestExp ? "Pokedex--winner" : "Pokedex--loser"}>Total Exp: {props.totalExp}</p>
+      <p
+        style={{
+          order: props.row === 'top' ? '1' : '0'
+        }}
+        className={`Pokedex--exp ${props.highestExp ? "Pokedex--winner" : "Pokedex--loser"}`}>
+        Total Exp: {props.totalExp}
+      </p>
     </div>
   )
 }
